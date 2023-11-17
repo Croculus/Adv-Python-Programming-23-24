9e4574f751668712470799546e94dcd2582dd0b0
Croculus <awildsnappyboi@gmail.com>
2023-11-17 12:56:28 -0500

    11/15 changes

--- game/game.py
+++ game/game.py
@@ -1,22 +1,23 @@
-import pygame, sprites
+import pygame, sprites, random
 
 
 
-
+#initialization
 pygame.init()
-screen_width = 750
-screen_height = 450
+screen_width = 950
+screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 game_state = "start_menu"
 
+clock = pygame.time.Clock()
+FPS = 6
 player = sprites.Player()
-background = pygame.image.load("backgrounds/Phyton Game Corner.jpg")
 
 def draw_start_menu():
-    screen.fill((0, 0, 0))
+    screen.fill((0, 255, 0))
     font = pygame.font.SysFont('arial', 40)
     title = font.render('My Game', True, (255, 255, 255))
-    start_button = font.render('Start', True, (255, 255, 255))
+    start_button = font.render('Start (with space)', True, (255, 255, 255))
     screen.blit(title, (screen_width/2 - title.get_width()/2, screen_height/2 - title.get_height()/2))
     screen.blit(start_button, (screen_width/2 - start_button.get_width()/2, screen_height/2 + start_button.get_height()/2))
     pygame.display.update()
@@ -38,12 +39,21 @@ def shoot():
     pass
 
 while True: 
-
+   clock.tick(FPS)
    for event in pygame.event.get(): 
        if event.type == pygame.QUIT:
            pygame.quit()
            quit()
-
    if game_state == "start_menu":
        draw_start_menu()
-        
+       keys = pygame.key.get_pressed()
+       if keys[pygame.K_SPACE]:
+            game_state = "game"
+            game_over = False
+   if game_state == " game":
+        background = pygame.image.load("backgrounds/Phyton Game Corner 2.jpg")
+        player = pygame.image.load("sprites/Character1.png")
+        screen.blit(background, (0, 0))
+        screen.blit(player, (240, 365))
+        pygame.display.update()
+   #print(pygame.mouse.get_pos()) 250 425
--- game/sprites.py
+++ game/sprites.py
@@ -5,10 +5,10 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
        # Call the parent class (Sprite) constructor
        pygame.sprite.Sprite.__init__(self)
-
+       self.frame = 1  #1, 2, or 3
        # Create an image of the block, and fill it with a color.
        # This could also be an image loaded from the disk.
-       self.image = pygame.image.load("backgrounds/Phyton Game Corner 2.jpg") #placeholder image
+       self.image = pygame.image.load("sprites/Character1.png") 
        self.image.convert()
 
        # Fetch the rectangle object that has the dimensions of the image
@@ -16,6 +16,10 @@ class Player(pygame.sprite.Sprite):
        self.rect = self.image.get_rect()
 
     def update(self) -> None:
+        self.image = pygame.image.load("sprites/Character{}.png".format(self.frame))
+        self.frame += 1
+        if self.frame > 3:
+            self.frame %= 3
         pass
     #be sure to override update to change animation
 
@@ -38,7 +42,7 @@ class Ball(pygame.sprite.Sprite):
         pass
     #be sure to override update to change animation
 
-class Cart(pygame.sprite.Sprite):
+class Bar(pygame.sprite.Sprite):
 
     def __init__(self):
        # Call the parent class (Sprite) constructor
@@ -57,7 +61,7 @@ class Cart(pygame.sprite.Sprite):
         pass
     #be sure to override update to change animation
 
-class Hoop(pygame.sprite.Sprite):
+class Cart(pygame.sprite.Sprite):
 
     def __init__(self):
        # Call the parent class (Sprite) constructor
