4242aeca808f9a072191f8bf85c3cd67684ff8ce
Croculus <awildsnappyboi@gmail.com>
2023-11-26 16:43:59 -0500

    implented shooting mechanics, still needs fixing

--- game/game.py
+++ game/game.py
@@ -17,26 +17,11 @@ def draw_start_menu():
     screen.fill((0, 255, 0))
     font = pygame.font.SysFont('arial', 40)
     title = font.render('My Game', True, (255, 255, 255))
-    start_button = font.render('Start (with space)', True, (255, 255, 255))
+    start_button = font.render('Start (with y)', True, (255, 255, 255))
     screen.blit(title, (screen_width/2 - title.get_width()/2, screen_height/2 - title.get_height()/2))
     screen.blit(start_button, (screen_width/2 - start_button.get_width()/2, screen_height/2 + start_button.get_height()/2))
     pygame.display.update()
 
-#rough psudocode
-def shoot():
-    #frames to show player grabbing ball, going back to shooting position
-    #subtract 1 from rack
-    # pick a spot on the bar as the "hotspot"
-    # while key is pressed player jumps and bar moves
-    # if key is held for too long automatically jump
-    # player move down
-    # check deviation from hotspot, calculate %change of going in and determine whether to score
-    # ball moves through air
-    # if ball is made, show path to going in to hoop
-    # else then generate a random number to offset ball path
-    # reset bar
-    # if rack is empty, change rack
-    pass
 
 while True: 
    clock.tick(FPS)
@@ -47,13 +32,20 @@ while True:
    if game_state == "start_menu":
        draw_start_menu()
        keys = pygame.key.get_pressed()
-       if keys[pygame.K_SPACE]:
+       if keys[pygame.K_y]:
             game_state = "game"
+            background = pygame.image.load("backgrounds/Phyton Game Corner 2.jpg")
+            screen.blit(background, (0, 0)) 
             game_over = False
    if game_state == "game":
-        background = pygame.image.load("backgrounds/Phyton Game Corner 2.jpg")
-        player = pygame.image.load("sprites/Character1.png")
-        screen.blit(background, (0, 0))
-        screen.blit(player, (240, 365))
+        
+        player = sprites.Player()
+        player_group = pygame.sprite.Group()
+        player_group.add(player)
+        player_group.draw(screen)
+        player_group.update()
         pygame.display.update()
+        pygame.event.pump()
+        
+            
    #print(pygame.mouse.get_pos()) 250 425
--- game/sprites.py
+++ game/sprites.py
@@ -1,25 +1,45 @@
-import pygame
+import pygame, random, math
 
+#jumpshot is a parabola; a(x-h)^2 +k
+jumpshot_dur = 180 # length/k value
+jumpshot_peak = jumpshot_dur/2 #vertex/ h value
+jumpshot_a = (-1*jumpshot_dur)/math.pow(jumpshot_peak, 2) #a value required to start from original position
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
        # Call the parent class (Sprite) constructor
        pygame.sprite.Sprite.__init__(self) #start at 240, 365
-       self.frame = 1  #1, 2, or 3
-       # Create an image of the block, and fill it with a color.
-       # This could also be an image loaded from the disk.
+       
        self.image = pygame.image.load("sprites/Character1.png") 
-       self.image.convert()
 
        # Fetch the rectangle object that has the dimensions of the image
        # Update the position of this object by setting the values of rect.x and rect.y
        self.rect = self.image.get_rect()
+       self.x = 240 #initial x
+       self.y = 365 #initial y
+       self.rect.center = [self.x, self.y]
+
+       self.count = 0 #count of frames in which space is pressed
+       self.score= 0
 
     def update(self) -> None:
-        self.image = pygame.image.load("sprites/Character{}.png".format(self.frame))
-        self.frame += 1
-        if self.frame > 3:
-            self.frame %= 3
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            if self.count == 0:
+                self.image = pygame.image.load("sprites/Character2.png")
+                self.rect = self.image.get_rect()
+            self.count+=1
+            self.y = 365 - ((jumpshot_a*math.pow(self.count-jumpshot_peak, 2))+jumpshot_dur) # inital y - parabola function, lower value = going up
+            
+            
+            self.rect.center = [self.x,self.y]
+            
+        elif self.count > 0:
+            shot_percentage = (-100/(math.pow(jumpshot_peak,2)))*(math.pow(self.count-jumpshot_peak))+100
+            shot_make = random.choices([True, False],cum_weights=[shot_percentage, 100])
+            release = pygame.event.Event(pygame.USEREVENT+1, {'make': shot_make}) #send event, notify ball to move
+            pygame.event.post(release)
+            #keep 
         pass
     #be sure to override update to change animation
 
@@ -39,7 +59,11 @@ class Ball(pygame.sprite.Sprite):
        self.rect = self.image.get_rect()
 
     def update(self) -> None:
-        pass
+        release= pygame.event.get(pygame.USEREVENT+1) #check for event
+        if release[0].__getattribute__('make') == True:
+            #math for ball path
+            pass
+
     #be sure to override update to change animation
 
 class Bar(pygame.sprite.Sprite):
