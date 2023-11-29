92aee426993eb548b62037e0a21a10f4079ff581
Croculus <awildsnappyboi@gmail.com>
2023-11-29 09:39:56 -0500

    combined karls edits and mine

--- game/sprites.py
+++ game/sprites.py
@@ -5,6 +5,7 @@ import pygame, random, math, time
 jumpshot_dur = 100 # vertex y /k value
 jumpshot_peak = jumpshot_dur/2 #vertex x / h value
 jumpshot_a = (-1*jumpshot_dur)/math.pow(jumpshot_peak, 2) #a value required to start from original position
+
 class Player(pygame.sprite.Sprite):
 
     def __init__(self):
@@ -42,9 +43,8 @@ class Player(pygame.sprite.Sprite):
         elif self.count > 1:
             #release data
             if self.sent_data == False: # checks if data has been sent then quits
-                modifier = 30 #raw number to reduce chance of making shots
+                 #raw number to reduce chance of making shots
                 shot_percentage = (-100/(math.pow(jumpshot_peak,2)))*(math.pow(self.count-jumpshot_peak, 2))+100
-                shot_percentage -= modifier
                 shot_make = random.choices([True, False],cum_weights=[shot_percentage, 100])
                 release = pygame.event.Event(pygame.USEREVENT+1, {'make': shot_make, 'x': self.x, 'y': self.y}) #send event, notify ball to move
                 pygame.event.post(release)
@@ -81,21 +81,42 @@ class Ball(pygame.sprite.Sprite):
        # Create an image of the block, and fill it with a color.
        # This could also be an image loaded from the disk.
        self.image = pygame.image.load("sprites/ball.png") #placeholder image
-       self.image.convert()
+       self.image.convert_alpha()
 
        # Fetch the rectangle object that has the dimensions of the image
        # Update the position of this object by setting the values of rect.x and rect.y
        self.rect = self.image.get_rect()
+       self.x = 310
+       self.y = 270
+       self.count=1
+       self.in_motion = False
 
     def update(self) -> None:
+        if self.in_motion == True:
+            if self.count%3 == 0:
+                self.image = pygame.transform.smoothscale_by(self.image, 0.99)
+            #self.image = pygame.transform.scale(self.image, (self.image.get_width()*(0.9999), self.image.get_height()*(0.9999)))
+            self.rect = self.image.get_rect()
+            self.count+=4
+            self.x+=4
+            self.y = 270 + (0.01*(self.count)*(self.count-170))
+            self.rect.center = [self.x, self.y]
+            if self.count >= 170: # reset
+                self.image = pygame.image.load("sprites/ball.png")
+                self.rect = self.image.get_rect()
+                self.x = 310
+                self.y = 270
+                self.in_motion = False
+
         release= pygame.event.get(pygame.USEREVENT+1) #get for event
         if len(release) == 0: #check if event is present (will cause error otherwise)
             return
         else:
+            self.count = 1
+            self.in_motion = True
             print('true')
-            x = release[0].dict['x']+25
-            y = release[0].dict['y']-155
-            self.rect.center= [x,y]
+
+            self.rect.center= [self.x,self.y]
             #math for ball path
             pass
 
