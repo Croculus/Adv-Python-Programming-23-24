dbe54273aa45a44c13baa0e6a7c0d8de6ac4aabf
Croculus <awildsnappyboi@gmail.com>
2023-12-04 10:48:51 -0500

    score and timer done, all we need now is an endgame and optionally some shot feedback

--- game/game.py
+++ game/game.py
@@ -10,7 +10,6 @@ screen_width = 950
 screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 game_state = "start_menu"
-
 clock = pygame.time.Clock()
 FPS = 60
 player = sprites.Player()
@@ -23,9 +22,11 @@ def draw_start_menu():
 
 player = sprites.Player()
 ball = sprites.Ball()
+scoreboard = sprites.Scoreboard()
 player_group = pygame.sprite.Group()
 player_group.add(player)
 player_group.add(ball)
+player_group.add(scoreboard)
 while True: 
    clock.tick(FPS)
    for event in pygame.event.get(): 
@@ -39,21 +40,21 @@ while True:
             game_state = "game"
             game_over = False
    if game_state == "game":
-        green_frames = 0
-          
-        if event.type == pygame.USEREVENT+2 or green_frames > 0:
+        if event.type == pygame.USEREVENT+2:
             background = pygame.image.load("backgrounds/Green Corner 2.png")
-            green_frames = 5
-        elif green_frames > 0:
-            pass
         else:
             background = pygame.image.load("backgrounds/Phyton Game Corner 2.jpg")
+        if event.type == pygame.USEREVENT+4:
+            game_state = 'endgame'
         screen.blit(background, (0, 0)) 
         player_group.update()
         player_group.draw(screen)
         #print(pygame.mouse.get_pos())
-        pygame.display.update()
+        pygame.display.update()   
         pygame.event.pump()
+   if game_state == 'endgame':
+       pygame.quit()
+       quit()
         
             
    #print(pygame.mouse.get_pos()) 480, 270
